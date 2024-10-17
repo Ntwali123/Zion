72730b1a733b827bb7a7e834821e68760611cdda
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2024-10-17 21:36:14 +0200

    new file added

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -32,7 +32,6 @@ const AuthForm = ({ type }: { type: string }) => {
 
   const formSchema = authFormSchema(type);
 
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -41,13 +40,10 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   });
 
-  // 2. Define a submit handler.
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
 
     try {
-      // Sign up with Appwrite & create plaid token
-
       if (type === "sign-up") {
         const userData = {
           firstName: data.firstName!,
